a9607c61f29a010067346eca09f77825f8cb7171
Gwartney <davidg@boundary.com>
2016-02-29 14:29:38 -0800

    Removed unused imports

--- src/main/java/com/bmc/truesight/pulse/plugin/JVM.java
+++ src/main/java/com/bmc/truesight/pulse/plugin/JVM.java
@@ -1,20 +1,7 @@
 package com.bmc.truesight.pulse.plugin;
 
-import com.boundary.plugin.sdk.PluginJSON;
 import com.boundary.plugin.sdk.PluginRunner;
 import com.boundary.plugin.sdk.jmx.JMXPlugin;
-import com.boundary.plugin.sdk.jmx.JMXPluginConfiguration;
-import com.boundary.plugin.sdk.jmx.MBeanEntry;
-import com.boundary.plugin.sdk.jmx.MBeanMap;
-import com.google.gson.Gson;
-import com.google.gson.JsonParseException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.File;
-import java.io.IOException;
-
-import java.util.ArrayList;
 
 public class JVM extends JMXPlugin {
 
